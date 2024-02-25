396d3a31bfe283752033bb59e108375acd40f4e8
Brajesh <brajeshkumarsah2010@gmail.com>
2024-02-25 22:15:00 +0530

    commited

---
+++ src/test/java/package2/Class3Test.java
@@ -0,0 +1,25 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class Class3Test {
+	@Test
+	public void methodNew1() {
+		System.out.println("methodNew1 is Exicute");
+	}
+
+	@Test
+	public void methodNew2() {
+		System.out.println("methodNew2 is Exicute");
+	}
+
+	@Test
+	public void methodNew3() {
+		System.out.println("methodNew3 is Exicute");
+	}
+
+	@Test
+	public void methodNew4() {
+		System.out.println("methodNew4 is Exicute");
+	}
+}
---
+++ src/test/java/package2/Class4Test.java
@@ -0,0 +1,25 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class Class4Test {
+	@Test
+	public void methodeNew5() {
+		System.out.println("methodeNew5 is Exicute");
+	}
+
+	@Test
+	public void methodeNew6() {
+		System.out.println("methodeNew6 is Exicute");
+	}
+
+	@Test
+	public void methodeNew7() {
+		System.out.println("methodeNew7 is Exicute");
+	}
+
+	@Test
+	public void methodeNew8() {
+		System.out.println("methodeNew8 is Exicute");
+	}
+}
